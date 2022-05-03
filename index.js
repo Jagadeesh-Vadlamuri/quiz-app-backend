7456c7ac11e7a8eb0005575079327ca8e2796184
Jagadeesh-Vadlamuri <jagadeesh.jacky1001@gmail.com>
2022-05-03 14:59:25 +0530

    quiz-app

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const URI = "mongodb+srv://QuizApp:QuizApp@cluster0.ae3vz.mongodb.net/quizapp?retryWrites=true&w=majority";
 
